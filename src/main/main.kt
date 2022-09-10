11396d19d417df1dba76e8a39d9f6f98c7f0e544
Ekaterina Efremova <katerina.usatykh@gmail.com>
2022-09-10 16:42:17 +0300

    add formula

--- src/main/main.kt
+++ src/main/main.kt
@@ -4,13 +4,35 @@ fun main() {
     val seconds = 1200
     val text = "Был(а) в сети"
 
-    var agoToText = when (seconds) {
-        in 0 < ... < 60 -> "только что"
-        in 61 < ... < 60 * 60 -> "x минут назад"
-        in 60 * 60 + 1 < ... < 24 * 60 * 60 -> "x часов назад"
-        in 24 * 60 * 60 < ... < 48 * 60 * 60 -> "вчера"
-        in 48 * 60 * 60 < ... < 72 * 60 * 60 -> "позавчера"
-        in 72 * 60 * 60 < ... -> "давно"
+    fun agoToText = when (seconds) {
+        in 0 < ... < 60 -> "$text только что"
+        in 61 < ... < 60 * 60 -> "$text $timeToMinutes назад"
+        in 60 * 60 + 1 < ... < 24 * 60 * 60 -> "$text $timeToHours назад"
+        in 24 * 60 * 60 < ... < 48 * 60 * 60 -> "$text вчера"
+        in 48 * 60 * 60 < ... < 72 * 60 * 60 -> "$text позавчера"
+        else -> "$text давно"
     }
+
+    fun timeToMinutes(seconds: Int) {
+        val minutes = seconds / 60
+        return when {
+            minutes % 10 == 1 -> "минуту"
+            minutes % 10 in 2 < ... < 4 -> "минуты"
+            else -> "минут"
+        }
+
+    }
+
+    fun timeToHours(minutes: Int) {
+        val hour = minutes / 60
+        return when {
+            hour % 10 == 1 -> "час"
+            hour % 10 in 2 < ... < 4 -> "часа"
+            else -> "часов"
+        }
+
+    }
+
+    println(agoToText())
 }
 
