7c40480889096b2ed33b7f7e9087fe9fc6b64eec
Ekaterina Efremova <katerina.usatykh@gmail.com>
2022-09-11 16:27:01 +0300

    edit main

--- src/main/main.kt
+++ src/main/main.kt
@@ -3,36 +3,33 @@ package ru.netology
 fun main() {
     val seconds = 1200
     val text = "Был(а) в сети"
-
-    fun agoToText = when (seconds) {
-        in 0 < ... < 60 -> "$text только что"
-        in 61 < ... < 60 * 60 -> "$text $timeToMinutes назад"
-        in 60 * 60 + 1 < ... < 24 * 60 * 60 -> "$text $timeToHours назад"
-        in 24 * 60 * 60 < ... < 48 * 60 * 60 -> "$text вчера"
-        in 48 * 60 * 60 < ... < 72 * 60 * 60 -> "$text позавчера"
-        else -> "$text давно"
-    }
-
-    fun timeToMinutes(seconds: Int) {
-        val minutes = seconds / 60
-        return when {
-            minutes % 10 == 1 -> "минуту"
-            minutes % 10 in 2 < ... < 4 -> "минуты"
-            else -> "минут"
-        }
-
-    }
-
-    fun timeToHours(minutes: Int) {
-        val hour = minutes / 60
-        return when {
-            hour % 10 == 1 -> "час"
-            hour % 10 in 2 < ... < 4 -> "часа"
-            else -> "часов"
-        }
-
-    }
-
-    println(agoToText())
+    println(agoToText(seconds, text))
 }
 
+fun agoToText(seconds: Int, text: String) = when (seconds) {
+    in 0..60 -> "$text только что"
+    in 61..60 * 60 -> "$text ${timeToMinutes(seconds)} назад"
+    in 60 * 60 + 1..24 * 60 * 60 -> "$text ${timeToHours(seconds)} назад"
+    in 24 * 60 * 60..48 * 60 * 60 -> "$text вчера"
+    in 48 * 60 * 60..72 * 60 * 60 -> "$text позавчера"
+    else -> "$text давно"
+}
+
+fun timeToMinutes(seconds: Int): String {
+    val minutes = seconds / 60
+    return when {
+        minutes % 10 == 1 -> "минуту"
+        minutes % 10 in 2..4 -> "минуты"
+        minutes in 11..14 -> "минут"
+        else -> "минут"
+    }
+}
+
+fun timeToHours(minutes: Int): String {
+    val hour = minutes / 60
+    return when {
+        hour % 10 == 1 -> "час"
+        hour % 10 in 2..4 -> "часа"
+        else -> "часов"
+    }
+}
